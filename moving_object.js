73d10116c85a5d71a064522d14b3878022376fe3
cookb <cookb@mit.edu>
2013-09-13 15:23:22 -0700

    split files up & refactored

--- moving_object.js
+++ moving_object.js
@@ -8,21 +8,19 @@
 	MovingObject.prototype.size = {width: 0, height: 0};
 	MovingObject.prototype.rotation = 0;
 	
+	MovingObject.prototype.screenWrap = function (axis, diameter, screenDim) {
+		if (this.position[axis] + (diameter / 2) <= 0 ) {
+			this.position[axis] = screenDim + (diameter / 2)
+		} else if (this.position[axis] - (diameter / 2) >= screenDim ) {
+			this.position[axis] = 0 - (diameter / 2)
+		}
+	};
+	
 	MovingObject.prototype.update = function () {
 		this.rotation %= 360;
 		this.position["x"] += this.velocity["x"];
 		this.position["y"] += this.velocity["y"];
-	
-		if (this.position["x"] + (this.size["width"] / 2) <= 0 ) {
-			this.position["x"] = screenWidth + (this.size["width"] / 2)
-		} else if (this.position["x"] - (this.size["width"] / 2) >= screenWidth ) {
-			this.position["x"] = 0 - (this.size["width"] / 2)
-		}
-
-		if (this.position["y"] + (this.size["height"] / 2) <= 0 ) {
-			this.position["y"] = screenHeight + (this.size["height"] / 2)
-		} else if (this.position["y"] - (this.size["height"] / 2) >= screenHeight ) {
-			this.position["y"] = 0 - (this.size["height"] / 2)
-		}
+		this.screenWrap("x", this.size["width"], screenWidth);
+		this.screenWrap("y", this.size["height"], screenHeight);
 	};
 })(this);	
